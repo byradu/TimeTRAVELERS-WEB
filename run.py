5ee3c50ba758efe7a09d68a57caf3dfe52f66f67
Zaharioaei Radu <44706212+byradu@users.noreply.github.com>
2021-04-14 01:28:29 +0300

    update de parsare date la afisare

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template,url_for,redirect
+from flask import Flask,render_template,url_for,redirect,request
+from jinja2.utils import generate_lorem_ipsum
 from forms import inputText
 
 app = Flask(__name__)
@@ -8,12 +9,14 @@ app.config['SECRET_KEY'] = 'd1c8efd030c6d6c85a46bde85c2597805cf6c18bf1f40dbbfb7f
 def home():
     form = inputText()
     if form.validate_on_submit():
-        return redirect(url_for('results'))
+        return redirect(url_for('results',userInput=form.inputText.data))
     return render_template('index.html',form = form)
 
 @app.route('/results/')
 def results():
-    return render_template('results.html')
+    data = request.args.get('userInput')
+    #prelucrare data
+    return render_template('results.html',data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
