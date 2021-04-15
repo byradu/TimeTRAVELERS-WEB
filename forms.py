f48679ff37222d926c1c98d24f033e86236fb166
Zaharioaei Radu <44706212+byradu@users.noreply.github.com>
2021-04-16 01:51:06 +0300

    upload_fisiere

--- forms.py
+++ forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField,FileAllowed
 from wtforms import TextAreaField,SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired,Optional
 
 class inputText(FlaskForm):
-    inputText = TextAreaField('Insert text to process',validators=[DataRequired()])
+    inputText = TextAreaField('Insert text to process',validators=[Optional()])
+    fileInput = FileField('Insert your file here',validators=[Optional(),FileAllowed(['pdf','txt'])] )
     submit = SubmitField('Start!')
--- run.py
+++ run.py
@@ -1,22 +1,41 @@
 from flask import Flask,render_template,url_for,redirect,request
-from jinja2.utils import generate_lorem_ipsum
 from forms import inputText
+import secrets,os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd1c8efd030c6d6c85a46bde85c2597805cf6c18bf1f40dbbfb7fc740d38b1113'
+
+def save(f):
+    random_hex = secrets.token_hex(8)
+    _,f_ext = os.path.splitext(f.filename)
+    saveF = random_hex + f_ext
+    filepath = os.path.join(app.root_path,saveF)
+    f.save(filepath)
+    return filepath
 
 @app.route('/',methods = ['POST','GET'])
 def home():
     form = inputText()
     if form.validate_on_submit():
-        return redirect(url_for('results',userInput=form.inputText.data))
+        print(form.fileInput.data)
+        print(form.inputText.data)
+        
+        #prelucrare
+        if form.fileInput.data != None:#daca e poza
+            to_remove_after_process_it = save(form.fileInput.data)#salvare pdf sau txt
+            return redirect(url_for('results',userInput=to_remove_after_process_it,mode=0))
+        elif form.inputText.data != '':#atunci e input manual
+            return redirect(url_for('results',userInput=form.inputText.data,mode=1))
     return render_template('index.html',form = form)
 
 @app.route('/results/')
 def results():
+    mode = request.args.get('mode')
     data = request.args.get('userInput')
     #prelucrare data
+    # if mode == 1:
     return render_template('results.html',data=data)
-
+    # else :
+        # return render_template('results.html',data="....")
 if __name__ == '__main__':
     app.run(debug=True)
