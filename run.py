83ebdc9879ce34cd67236700a9fb01163c5e8f21
Zaharioaei Radu <44706212+byradu@users.noreply.github.com>
2021-05-24 00:31:20 +0300

    pdf and other stuff finished

--- run.py
+++ run.py
@@ -6,6 +6,11 @@ from templates.pysyntime import SynTime, syntime
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd1c8efd030c6d6c85a46bde85c2597805cf6c18bf1f40dbbfb7fc740d38b1113'
 
+
+IMAGE_FOLDER=os.path.join('static')
+app.config['UPLOAD_FOLDER']=IMAGE_FOLDER
+loading_gif=os.path.join(app.config['UPLOAD_FOLDER'],'spinner2.gif')
+
 evenimente = []
 numepdf = ''
 
@@ -33,7 +38,7 @@ def getTextFromPdf(numePdf):
     images = convert_from_path(numePdf,poppler_path=r'C:\Program Files\poppler-0.68.0\bin')
     extractedText = ''
     for i in images:
-        extractedText = extractedText + image_to_string(i,lang='ron+equ',config="--psm 6") + ' '
+        extractedText = extractedText + image_to_string(i,lang='ron+equ+eng',config="--psm 6") + ' '
     
     return extractedText
 
@@ -46,33 +51,42 @@ def createMarkedPdf(nume,functionEvent): #salvarea pdf-ului marcat
     from pdf2image import convert_from_path
     import pytesseract
     from pytesseract import Output
+    from PIL import Image
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     images = convert_from_path(nume,poppler_path=r'C:\Program Files\poppler-0.68.0\bin')
     try:
         os.remove('result.pdf')        
     except :
         pass
+    ok = 0
+    first_page = 0
+    restOfThePages = []
     for i in images:
         img = cv.cvtColor(np.array(i),cv.COLOR_RGB2BGR)
         d = pytesseract.image_to_data(img,output_type=Output.DICT,lang='eng+ron+equ',config="--psm 6")
         boxes = len(d['level'])
-        for i in range(boxes):
-            for e in functionEvent:
-                if e in d['text'][i]:
-                    (x,y,w,h) = (d['left'][i],d['top'][i],d['width'][i],d['height'][i])
+        for e in functionEvent:
+            for j in range(boxes):
+                if e in d['text'][j]:
+                    (x,y,w,h) = (d['left'][j],d['top'][j],d['width'][j],d['height'][j])
                     cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-                elif i+1<boxes:
-                    if e in (d['text'][i]+' '+d['text'][i+1] or d['text'][i]+d['text'][i+1]):
-                        (x,y,w,h) = (max(d['left'][i],d['left'][i+1]),max(d['top'][i],d['top'][i+1]),max(d['width'][i],d['width'][i+1]),max(d['height'][i],d['height'][i+1]))
+                elif j+1<boxes:
+                    if e in (d['text'][j]+' '+d['text'][j+1] or d['text'][j]+d['text'][j+1]):
+                        (x,y,w,h) = (min(d['left'][j],d['left'][j+1]),max(d['top'][j],d['top'][j+1]),max(d['width'][j],d['width'][j+1]),max(d['height'][j],d['height'][j+1]))
                         cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-        pdf = pytesseract.image_to_pdf_or_hocr(img,extension='pdf')
-        with open('results.pdf','a+b') as f:
-            f.write(pdf)
+        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        im_pil = Image.fromarray(img)
+        restOfThePages.append(im_pil)
+        if ok == 0:
+            ok = 1
+            first_page = im_pil
+    first_page.save('results.pdf',"PDF",resolution=100.0,save_all=True,append_images=restOfThePages[1:])
 
 
 
 @app.route('/',methods = ['POST','GET'])
 def home():
+    
     form = inputText()
     if form.validate_on_submit():
         
@@ -83,10 +97,11 @@ def home():
         elif form.inputText.data != '':#atunci e input manual
             # print(form.inputText.data)
             return redirect(url_for('results',userInput=form.inputText.data))
-    return render_template('index.html',form = form)
+    return render_template('index.html',form = form,loading_gif=loading_gif)
 
 @app.route('/results/',methods =  ['POST','GET'])
 def results():
+    
     global evenimente,numepdf
     syntime = SynTime()
     date = datetime.today().strftime("%d-%m-%Y")
@@ -103,7 +118,7 @@ def results():
             except :
                 timeMLText = syntime.extractTimexFromText(form.inputText.data, date)
             return redirect(url_for('output',data=timeMLText,pdf = data))
-        return render_template('results.html',data=textdinpdf,form=form)
+        return render_template('results.html',data=textdinpdf,form = form,loading_gif=loading_gif)
     elif data != '':
         try:            
             timeMLText,evenimente = syntime.extractTimexFromText(data, date)
@@ -127,10 +142,11 @@ def output():
         except:
             pass
     events = dict(sorted(events.items()))
-    createMarkedPdf(numepdf,evenimente)
+    print(evenimente)
     if numepdf == '':
         return render_template('output.html',data=data,pdf=0,events=events)
     else:    
+        createMarkedPdf(numepdf,evenimente)
         return render_template('output.html',data=data,pdf=1,nume='results.pdf',events=events)
 
 @app.route('/download', methods=['GET'])
@@ -138,6 +154,12 @@ def download():
     file="templates/pysyntime/resource/outputXML.xml"
     print(os.getcwd())
     return send_file(file,mimetype='text/xml',as_attachment=True)
+
+@app.route('/downloadpdf',methods=['GET'])
+def downloadpdf():
+    file="results.pdf"
+    print(os.getcwd())
+    return send_file(file,mimetype='application/pdf',as_attachment=True)
 
 @app.route('/pdf/<numepdf>')
 def showpdf(numepdf):
